8c259161d6d1b9100a54f1167fcb8095a258161a
Fariba <fariba.yousefi@gmail.com>
2021-09-24 14:40:38 +0100

    added the main function

--- mixed_input_train.py
+++ mixed_input_train.py
@@ -15,49 +15,7 @@ import focal_loss
 import datetime as dt
 import sys
 
-# Specify the directory where the training and validation data are stored
-data_dir = sys.argv[1]
 
-# Enable XLA
-tf.config.optimizer.set_jit(True)
-
-# Limit GPU memory growth
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-  try:
-    # Currently, memory growth needs to be the same across GPUs
-    for gpu in gpus:
-      tf.config.experimental.set_memory_growth(gpu, True)
-    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-  except RuntimeError as e:
-    # Memory growth must be set before GPUs have been initialized
-    print(e)
-
-# We want to input the following data into the model
-# - Light curve data: Time-series data, measurements of an objects brightness over time
-# - Contextual data: Position, proximity to any galaxies
-
-# Load preprocessed data
-X_train_time = np.load('data/{}/train_time_features.npy'.format(data_dir))
-X_train_contextual = np.load('data/{}/train_contextual_features.npy'.format(data_dir))
-y_train = np.load('data/{}/train_labels.npy'.format(data_dir))
-X_train = (X_train_time, X_train_contextual)
-
-X_val_time = np.load('data/{}/validation_time_features.npy'.format(data_dir))
-X_val_contextual = np.load('data/{}/validation_contextual_features.npy'.format(data_dir))
-y_val = np.load('data/{}/validation_labels.npy'.format(data_dir))
-X_val = (X_val_time, X_val_contextual)
-
-# Print input and output dimensions
-rnn_input_shape = (X_train[0].shape[1], X_train[0].shape[2])
-print('RNN input shape ', rnn_input_shape)
-
-dnn_input_shape = (X_train[1].shape[1])
-print('Dense network input shape ',dnn_input_shape)
-
-output_shape = y_train.shape[1]
-print('Output shape ', output_shape)
 
 # Create a GRU RNN model to take in mixed inputs
 def create_gru_model(rnn_input_shape, dnn_input_shape, output_shape, 
@@ -140,7 +98,8 @@ def plot_history(history):
     return plt
 
 # Train a grid of models
-def train_model_grid(rnn_type, class_weights, param_grid, metrics, save_file_suffix, verbose):
+def train_model_grid(rnn_type, rnn_input_shape, dnn_input_shape, output_shape, mask_val, X_train, 
+                        y_train, X_val, y_val, epochs, class_weights, param_grid, metrics, save_file_suffix, verbose):
 
     print('Model configurations: ', len(param_grid))
 
@@ -190,13 +149,14 @@ def train_model_grid(rnn_type, class_weights, param_grid, metrics, save_file_suf
                                                         verbose=0)
 
         if rnn_type == 'GRU':
-            model = create_gru_model(rnn_input_shape=rnn_input_shape, dnn_input_shape=dnn_input_shape, output_shape=output_shape,
-                           rnn_dim=rnn_dim, final_dense_dim=final_dense_dim, dropout=dropout,
-                           regularization=reg_l2, mask_value=mask_val)
+            model = create_gru_model(rnn_input_shape=rnn_input_shape, dnn_input_shape=dnn_input_shape, 
+                                    output_shape=output_shape, rnn_dim=rnn_dim, 
+                                    final_dense_dim=final_dense_dim, dropout=dropout, 
+                                    regularization=reg_l2, mask_value=mask_val)
         elif rnn_type == 'LSTM':
-            model = create_lstm_model(rnn_input_shape=rnn_input_shape, dnn_input_shape=dnn_input_shape, output_shape=output_shape,
-                           rnn_dim=rnn_dim, final_dense_dim=final_dense_dim, dropout=dropout,
-                           regularization=reg_l2, mask_value=mask_val)
+            model = create_lstm_model(rnn_input_shape=rnn_input_shape, dnn_input_shape=dnn_input_shape, 
+                                    output_shape=output_shape, rnn_dim=rnn_dim, 
+                                    final_dense_dim=final_dense_dim, dropout=dropout, regularization=reg_l2, mask_value=mask_val)
 
 
         # Initialize optimizer, use the Adam optimizer
@@ -253,62 +213,118 @@ def train_model_grid(rnn_type, class_weights, param_grid, metrics, save_file_suf
     # Write results to .csv file
     results.to_csv(root + '/training_results.csv', index=0)
 
-# Make class weights to account for class imbalance
-y_labels = [np.argmax(y) for y in y_train]
-class_size = [y_labels.count(c) for c in np.unique(y_labels)]
-class_weights = np.array([sum(class_size)/c for c in class_size])
+def main():
 
-# Scale weights
-class_weights = np.array(class_weights) / len(list(class_weights))
-print('Class weights: ', class_weights)
+    # Specify the directory where the training and validation data are stored
+    data_dir = sys.argv[1]
 
-# Convert to dictionary
-class_weights = dict(zip(np.unique(y_labels), np.array(class_weights)))
+    # Enable XLA
+    tf.config.optimizer.set_jit(True)
 
-# Define parameters that won't change
-verbose, epochs = 1, 5
-metrics = [tf.keras.metrics.CategoricalAccuracy(),
-   rnntools.f1]
+    # Limit GPU memory growth
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
 
-# Define hyperparameter grid
-batch_sizes = [32]
-learning_rates = [1e-4]
-rnn_dims = [25]
-dropouts = [0.5]
-reg_l2s = [0.01]
-final_dense_dims = [25]
-gammas = [1.0]
-mask_val = -99.
+    # We want to input the following data into the model
+    # - Light curve data: Time-series data, measurements of an objects brightness over time
+    # - Contextual data: Position, proximity to any galaxies
 
-param_grid_fl = ParameterGrid({'batch_size':batch_sizes,
-                            'learning_rate':learning_rates,
-                            'rnn_dim':rnn_dims,
-                            'dropout':dropouts,
-                            'regularization_l2':reg_l2s,
-                            'final_dense_dim':final_dense_dims,
-                            'gamma':gammas})
+    # Load preprocessed data
+    X_train_time = np.load('data/{}/train_time_features.npy'.format(data_dir))
+    X_train_contextual = np.load('data/{}/train_contextual_features.npy'.format(data_dir))
+    y_train = np.load('data/{}/train_labels.npy'.format(data_dir))
+    X_train = (X_train_time, X_train_contextual)
 
-param_grid_ce = ParameterGrid({'batch_size':batch_sizes,
-                            'learning_rate':learning_rates,
-                            'rnn_dim':rnn_dims,
-                            'dropout':dropouts,
-                            'regularization_l2':reg_l2s,
-                            'final_dense_dim':final_dense_dims,
-                            'gamma':[0.0]})
+    X_val_time = np.load('data/{}/validation_time_features.npy'.format(data_dir))
+    X_val_contextual = np.load('data/{}/validation_contextual_features.npy'.format(data_dir))
+    y_val = np.load('data/{}/validation_labels.npy'.format(data_dir))
+    X_val = (X_val_time, X_val_contextual)
+
+    # Print input and output dimensions
+    rnn_input_shape = (X_train[0].shape[1], X_train[0].shape[2])
+    print('RNN input shape ', rnn_input_shape)
+
+    dnn_input_shape = (X_train[1].shape[1])
+    print('Dense network input shape ',dnn_input_shape)
+
+    output_shape = y_train.shape[1]
+    print('Output shape ', output_shape)
+
+    # Define hyperparameter grid
+    batch_sizes = [32]
+    learning_rates = [1e-4]
+    rnn_dims = [25]
+    dropouts = [0.5]
+    reg_l2s = [0.01]
+    final_dense_dims = [25]
+    gammas = [1.0]
+    mask_val = -99.
+
+    # Define parameters that won't change
+    verbose, epochs = 1, 5
+    metrics = [tf.keras.metrics.CategoricalAccuracy(),
+    rnntools.f1]
+    # Make class weights to account for class imbalance
+    y_labels = [np.argmax(y) for y in y_train]
+    class_size = [y_labels.count(c) for c in np.unique(y_labels)]
+    class_weights = np.array([sum(class_size)/c for c in class_size])
+
+    # Scale weights
+    class_weights = np.array(class_weights) / len(list(class_weights))
+    print('Class weights: ', class_weights)
+
+    # Convert to dictionary
+    class_weights = dict(zip(np.unique(y_labels), np.array(class_weights)))
+
+    param_grid_fl = ParameterGrid({'batch_size':batch_sizes,
+                                'learning_rate':learning_rates,
+                                'rnn_dim':rnn_dims,
+                                'dropout':dropouts,
+                                'regularization_l2':reg_l2s,
+                                'final_dense_dim':final_dense_dims,
+                                'gamma':gammas})
+
+    param_grid_ce = ParameterGrid({'batch_size':batch_sizes,
+                                'learning_rate':learning_rates,
+                                'rnn_dim':rnn_dims,
+                                'dropout':dropouts,
+                                'regularization_l2':reg_l2s,
+                                'final_dense_dim':final_dense_dims,
+                                'gamma':[0.0]})
 
 
-# Train GRU models with focal loss
-train_model_grid(rnn_type='GRU', class_weights=class_weights,
-    param_grid=param_grid_fl, metrics=metrics, save_file_suffix='-gru-fl', verbose=verbose)
+    # Train GRU models with focal loss
+    train_model_grid(rnn_type='GRU', rnn_input_shape=rnn_input_shape, dnn_input_shape=dnn_input_shape, 
+                    output_shape=output_shape, mask_val=mask_val, X_train=X_train, y_train=y_train,
+                    X_val=X_val, y_val=y_val, epochs=epochs, class_weights=class_weights, 
+                    param_grid=param_grid_fl, metrics=metrics, save_file_suffix='-gru-fl', verbose=verbose)
 
-# Train LSTM models with focal loss
-train_model_grid(rnn_type='LSTM', class_weights=class_weights,
-    param_grid=param_grid_fl, metrics=metrics, save_file_suffix='-lstm-fl', verbose=verbose)
+    # Train LSTM models with focal loss
+    train_model_grid(rnn_type='LSTM', rnn_input_shape=rnn_input_shape, dnn_input_shape=dnn_input_shape, 
+                     output_shape=output_shape, mask_val=mask_val, X_train=X_train, y_train=y_train,
+                     X_val=X_val, y_val=y_val, epochs=epochs, class_weights=class_weights, 
+                     param_grid=param_grid_fl, metrics=metrics, save_file_suffix='-lstm-fl', verbose=verbose)
 
-# Train GRU models with cross entropy
-train_model_grid(rnn_type='GRU', class_weights=class_weights,     
-    param_grid=param_grid_ce, metrics=metrics, save_file_suffix='-gru-wce', verbose=verbose)
+    # Train GRU models with cross entropy
+    train_model_grid(rnn_type='GRU', rnn_input_shape=rnn_input_shape, dnn_input_shape=dnn_input_shape, 
+                    output_shape=output_shape, mask_val=mask_val, X_train=X_train, y_train=y_train,
+                    X_val=X_val, y_val=y_val, epochs=epochs, class_weights=class_weights, 
+                    param_grid=param_grid_ce, metrics=metrics, save_file_suffix='-gru-wce', verbose=verbose)
 
-# Train LSTM models with cross entropy
-train_model_grid(rnn_type='LSTM', class_weights=class_weights,     
-    param_grid=param_grid_ce, metrics=metrics, save_file_suffix='-lstm-wce', verbose=verbose)
+    # Train LSTM models with cross entropy
+    train_model_grid(rnn_type='LSTM', rnn_input_shape=rnn_input_shape, dnn_input_shape=dnn_input_shape, 
+                    output_shape=output_shape, mask_val=mask_val, X_train=X_train, y_train=y_train,
+                    X_val=X_val, y_val=y_val, epochs=epochs, class_weights=class_weights, 
+                    param_grid=param_grid_ce, metrics=metrics, save_file_suffix='-lstm-wce', verbose=verbose)
+
+if __name__=='__main__':
+    main()
