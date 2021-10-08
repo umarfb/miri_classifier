619c2a1942ee6ae3dd6b551448436a65414d64a5
Fariba <fariba.yousefi@gmail.com>
2021-10-08 18:38:38 +0100

    changing prints to logging

--- model_evaluation.py
+++ model_evaluation.py
@@ -10,45 +10,42 @@ from rnntools import rnntools
 import focal_loss
 from sklearn.metrics import f1_score, roc_auc_score, accuracy_score, roc_curve
 import matplotlib.pyplot as plt
-import sys
 import click
+import logging
 
-# for arg in sys.argv:
-#     print(arg)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("model_evaluation")
 
-# Method to evaluate a model on test data
-def evaluate(model, X, y, batch_size, metrics):
+# # Method to evaluate a model on test data
+# def evaluate(model, X, y, batch_size):
+#     performance = rnntools.evaluate_model(model, X, y, batch_size)
 
-    performance = rnntools.evaluate_model(model, X, y, batch_size)
+#     # Add performance metrics to a dictionary
+#     score = {'loss':performance[0],
+#              'accuracy':performance[1],
+#              'f1_score':performance[2]}
 
-    # Add performance metrics to a dictionary
-    score = {'loss':performance[0],
-             'accuracy':performance[1],
-             'f1_score':performance[2]}
+#     return score
 
-    return score
+# # Method to make predictions, and save performanc metrics, takes in outputs of prediction probabilities
+# def evaluate_model_time(y_true, y_pred, labels, save_dir, plot_title, save_plot=False):
+#     yt = y_true.argmax(axis=1)
+#     yp = y_pred.argmax(axis=1)
 
-# Method to make predictions, and save performanc metrics, takes in outputs of prediction probabilities
-def evaluate_model_time(y_true, y_pred, labels, save_dir, plot_title, save_plot=False):
+#     # Calculate accuracy and f1-score
+#     f1 = f1_score(yt, yp, average='weighted')
+#     accuracy = accuracy_score(yt, yp)
 
-    yt = y_true.argmax(axis=1)
-    yp = y_pred.argmax(axis=1)
+#     # Create confusion matrix
+#     cm = rnntools.confusion_matrix(y_pred, y_true, labels, title=plot_title)
 
-    # Calculate accuracy and f1-score
-    f1 = f1_score(yt, yp, average='weighted')
-    accuracy = accuracy_score(yt, yp)
+#     if save_plot == True:
+#         cm.savefig(os.path.join(save_dir, 'time-evaluation/CM_{}.png').format(plot_title[8:]), format='png', dpi=300, bbox_inches='tight')
 
-    # Create confusion matrix
-    cm = rnntools.confusion_matrix(y_pred, y_true, labels, title=plot_title)
-
-    if save_plot == True:
-        cm.savefig(os.path.join(save_dir, 'time-evaluation/CM_{}.png').format(plot_title[8:]), format='png', dpi=300, bbox_inches='tight')
-
-    return f1, accuracy
+#     return f1, accuracy
 
 # Method to get model parameters
 def get_params(name):
-    
     param_vals = name.split('_')
     param_names = ['batch_size',
                    'dnn_dense_dim',
@@ -63,61 +60,59 @@ def get_params(name):
     
     return params
 
-# Method to preprocess data so that only a given number of epochs is retained
-def format_epoch(X, n_epochs, maxlen, mask_value=-99.0):
+# # Method to preprocess data so that only a given number of epochs is retained
+# def format_epoch(X, n_epochs, maxlen, mask_value=-99.0):
+#     N = int(n_epochs)
     
-    N = int(n_epochs)
+#     if N > maxlen:
+#         print('Error: number of epochs specified greater than maximum length of input time sequence, {} > {}'.format(N, maxlen))
+#         return
     
-    if N > maxlen:
-        print('Error: number of epochs specified greater than maximum length of input time sequence, {} > {}'.format(
-            N, maxlen))
-        return
-    
-    X = list(X)
-    X_epoch = [x[~(x[:,0] < 0)][:n_epochs] for x in X]
+#     X = list(X)
+#     X_epoch = [x[~(x[:,0] < 0)][:n_epochs] for x in X]
 
-    X_formatted = tf.keras.preprocessing.sequence.pad_sequences(X_epoch, padding='pre', value=mask_value,
-                                                                 dtype='float32', maxlen=maxlen)
+#     X_formatted = tf.keras.preprocessing.sequence.pad_sequences(X_epoch, padding='pre', value=mask_value,
+#                                                                  dtype='float32', maxlen=maxlen)
     
-    return X_formatted
+#     return X_formatted
 
-def multi_roc(yp, yt):
+# def multi_roc(yp, yt):
+#     # Get binary outputs for VS classification
+#     vs = yp[:,0]
+#     vs_probs = np.array([[p, 1-p] for p in vs])
+#     yt_vs = np.argmax(yt, axis=1)
+#     yt_vs[yt_vs >= 1] = 1
+#     vs_labels = yt_vs^1
     
-    # Get binary outputs for VS classification
-    vs = yp[:,0]
-    vs_probs = np.array([[p, 1-p] for p in vs])
-    yt_vs = np.argmax(yt, axis=1)
-    yt_vs[yt_vs >= 1] = 1
-    vs_labels = yt_vs^1
+#     # Get binary outputs for SN classification
+#     sn = yp[:,1]
+#     sn_probs = np.array([[p, 1-p] for p in sn])
+#     yt_sn = np.argmax(yt, axis=1)
+#     yt_sn[yt_sn != 1] = 0
+#     sn_labels = yt_sn
     
-    # Get binary outputs for SN classification
-    sn = yp[:,1]
-    sn_probs = np.array([[p, 1-p] for p in sn])
-    yt_sn = np.argmax(yt, axis=1)
-    yt_sn[yt_sn != 1] = 0
-    sn_labels = yt_sn
+#     # Get binary outputs for AGN classification
+#     agn = yp[:,2]
+#     agn_probs = np.array([[p, 1-p] for p in agn])
+#     yt_agn = np.argmax(yt, axis=1)
+#     yt_agn[yt_agn != 2] = 0
+#     yt_agn[yt_agn == 2] = 1
+#     agn_labels = yt_agn
     
-    # Get binary outputs for AGN classification
-    agn = yp[:,2]
-    agn_probs = np.array([[p, 1-p] for p in agn])
-    yt_agn = np.argmax(yt, axis=1)
-    yt_agn[yt_agn != 2] = 0
-    yt_agn[yt_agn == 2] = 1
-    agn_labels = yt_agn
+#     roc_vs = roc_curve(vs_labels, yp[:,0])
+#     roc_sn = roc_curve(sn_labels, yp[:,1])
+#     roc_agn = roc_curve(agn_labels, yp[:,2])
     
-    roc_vs = roc_curve(vs_labels, yp[:,0])
-    roc_sn = roc_curve(sn_labels, yp[:,1])
-    roc_agn = roc_curve(agn_labels, yp[:,2])
-    
-    return roc_vs, roc_sn, roc_agn
+#     return roc_vs, roc_sn, roc_agn
 
 @click.command()
 @click.option("-d", "--data-dir", required=True, help="Specify the directory where the data is stored")
 @click.option("-m", "--model_dir", required=True, help="Specify the directory where the model is stored")
 @click.option("-l", "--labels", required=True, multiple=True, help="Specify class labels - repeat this option for each class")
-def main(data_dir:str, model_dir:str, labels:List[str]):
 
-    print("Loading data ...")
+def main(data_dir:str, model_dir:str, labels:List[str]):
+    logger.info("Loading data ...")
+
     X_val_time = np.load('data/{}/validation_time_features.npy'.format(data_dir))
     X_val_contextual = np.load('data/{}/validation_contextual_features.npy'.format(data_dir))
     y_val = np.load('data/{}/validation_labels.npy'.format(data_dir))
@@ -132,26 +127,23 @@ def main(data_dir:str, model_dir:str, labels:List[str]):
     model_dir += '/'
     model_files = next(os.walk(model_dir))[1]
 
-
-    print(labels)
+    logger.info(f"class labels:{labels}")
 
     # Define metrics
-    metrics = [tf.keras.metrics.CategoricalAccuracy(), tf.keras.metrics.AUC(),
-    rnntools.f1]
+    metrics = [tf.keras.metrics.CategoricalAccuracy(), tf.keras.metrics.AUC(), rnntools.f1]
 
     # List to store evaluation results
     results = list()
 
-    print(y_val.shape, y_val.shape[0], y_val.shape[1])
+    logger.info(f"y_val shape: {y_val.shape}")
 
-    for i, m in enumerate(model_files):
-        
+    for model_file in model_files:
         # Get model parameters
-        params = get_params(m)
+        params = get_params(model_file)
 
         # Define path to models
-        model_path = os.path.join(model_dir, m, 'trained_model.h5')
-        weights_path = os.path.join(model_dir, m, 'model_weights/training/cp.ckpt')
+        model_path = os.path.join(model_dir, model_file, 'trained_model.h5')
+        weights_path = os.path.join(model_dir, model_file, 'model_weights/training/cp.ckpt')
         
         # Load model
         model = tf.keras.models.load_model(model_path, compile=False)
@@ -168,8 +160,6 @@ def main(data_dir:str, model_dir:str, labels:List[str]):
         # Expect_partial would silence the warnings.         
         # There are warnings because we are restoring a model that has training information but we are using it only for prediction and not training.
         model.load_weights(weights_path).expect_partial() 
-
-        
         
         # Evaluate model on validation set
         model_eval = rnntools.evaluate_model(model, X_val, y_val, batch_size=128, verbose=1)
@@ -180,7 +170,6 @@ def main(data_dir:str, model_dir:str, labels:List[str]):
                             average='macro')
         
         # Calulcate area under ROC curve
-
         if y_val.shape[1] == 2:
             auc_macro = roc_auc_score(np.argmax(y_val, axis=1),
                                 model.predict(X_val)[:,0])
